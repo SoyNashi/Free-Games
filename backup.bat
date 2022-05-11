60c3a760171551034b3c8750a93b34084b798d5c
SoyNashi <mrnilopand@gmail.com>
2022-05-11 10:28:25 +0200

    Update backup.bat

--- backup.bat
+++ backup.bat
@@ -1,7 +1,18 @@
 @echo off
+set log=readme.txt
 set a=Readme_%date:~-4,4%-%date:~-7,2%-%date:~-10,2%.txt
 set b=Log_%date:~-4,4%-%date:~-7,2%-%date:~-10,2%.txt
 set c=Glist_%date:~-4,4%-%date:~-7,2%-%date:~-10,2%.txt
+:createlog
+echo start.backup.%date% >> %log%
+echo var.check.a.%a%/var.check.b.%b%/var.check.c.%c%/
+echo file.created.%a%
+echo file.created.%b%
+echo file.created.%c%
+echo file.copied.readmemd.to.%a%
+echo file.copied.readmetxt.to.%b%
+echo file.copied.gamelistmd.to.%c%
+echo backup.completed
 :createfiles
 echo Backup Readme %date% > %a%
 echo Backup Log %date% > %b%
@@ -10,3 +21,7 @@ echo Backup Glist %date% > %c%
 copy "readme.md" "%a%"
 copy "readme.txt" "%b%"
 copy "gamelist.md" "%c%"
+:movefiles
+move %a% "/backup"
+move %b% "/backup"
+move %c% "/backup"
